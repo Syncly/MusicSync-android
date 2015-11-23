ad5082b080072877a5fde8930bc48a20515ca138
Arti Zirk <arti.zirk@gmail.com>
2015-11-23 19:29:38 +0200

    Added a listview and populated it with some data

--- app/src/main/java/ee/arti/musicsync/PlaylistsActivity.java
+++ app/src/main/java/ee/arti/musicsync/PlaylistsActivity.java
@@ -6,15 +6,46 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class PlaylistsActivity extends AppCompatActivity {
 
     private static final String TAG = "PlaylistsActivity";
+    private static final String TAG_PLAYLIST_NAME = "name";
+    private static final String TAG_PLAYLIST_STATUS = "status";
+    private static final String TAG_PLAYLIST_DEBUG = "debug_data";
+
+    // contains all the playlists
+    ArrayList<HashMap<String, String>> playlists = new ArrayList<HashMap<String, String>>();;
+    // converts array list to a suitable format for ListView
+    SimpleAdapter adapter;
+    // playlists list view
+    private ListView playlistsListView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_playilist_list);
+        setContentView(R.layout.activity_playlists);
+
+        HashMap<String, String> plitem = new HashMap<String, String>();
+        // add some dummy data
+        plitem.put(TAG_PLAYLIST_NAME, "Music I like");
+        plitem.put(TAG_PLAYLIST_DEBUG, "youtube:PLB5VrND_o3PgZNzNdohFDWE5BTFIPDImQ");
+        plitem.put(TAG_PLAYLIST_STATUS, "Syncing 0/136");
+        playlists.add(plitem);
+
+        // Create the adapter that will show our playlists in a scrollable list view
+        adapter=new SimpleAdapter(this, playlists, R.layout.playlist_element,
+                new String[] {TAG_PLAYLIST_NAME, TAG_PLAYLIST_DEBUG, TAG_PLAYLIST_STATUS},
+                new int[] {R.id.name, R.id.debug_data, R.id.status});
+
+        playlistsListView = (ListView) findViewById(R.id.playlistsList);
+        playlistsListView.setAdapter(adapter);
+
     }
 
     @Override
