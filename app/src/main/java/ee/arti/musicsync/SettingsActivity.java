3fae9541bef6ae16be4245f6c8db38e25a4deb31
Arti Zirk <arti.zirk@gmail.com>
2015-11-23 19:01:11 +0200

    Added some settings to SettingsActivity

--- app/src/main/java/ee/arti/musicsync/SettingsActivity.java
+++ app/src/main/java/ee/arti/musicsync/SettingsActivity.java
@@ -1,5 +1,6 @@
 package ee.arti.musicsync;
 
+import android.preference.PreferenceFragment;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -9,5 +10,18 @@ public class SettingsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_settings);
+
+        // inserts the default android settings fragments into the settings layout
+        getFragmentManager().beginTransaction().replace(android.R.id.content, new SettingsFragment()).commit();
+    }
+
+    public static class SettingsFragment extends PreferenceFragment
+    {
+        @Override
+        public void onCreate(final Bundle savedInstanceState)
+        {
+            super.onCreate(savedInstanceState);
+            addPreferencesFromResource(R.xml.settings);
+        }
     }
 }
