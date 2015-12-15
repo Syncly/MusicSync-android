c316776b5f7e3b4dadf6fdc98dfcd1557d626e35
Arti Zirk <arti.zirk@gmail.com>
2015-12-15 23:59:29 +0200

    Swipe down to refresh working

--- app/src/main/java/ee/arti/musicsync/PlaylistListActivity.java
+++ app/src/main/java/ee/arti/musicsync/PlaylistListActivity.java
@@ -1,13 +1,172 @@
 package ee.arti.musicsync;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.SimpleAdapter;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class PlaylistListActivity extends AppCompatActivity {
+
+    private static final String TAG = "SongsActivity";
+    private static final String TAG_SONG_TITLE = "title";
+    private static final String TAG_SONG_STATUS = "status";
+    private static final String TAG_SONG_ID = "_id";
+    private static final String TAG_PLAYLIST_ID = "playlist";
+
+    private String title; // Playlist name shown as this activitys title
+    private String playlist_id; //
+
+    // contains all the playlists
+    private ArrayList<HashMap<String, String>> songs = new ArrayList<>();;
+    // converts array list to a suitable format for ListView
+    private SimpleAdapter adapter;
+    // playlists list view
+    private ListView songsListView;
+
+    // updates spinner
+    private SwipeRefreshLayout swipeContainer;
+
+    // settings object
+    private SharedPreferences SP;
+
+    // internet suff
+    private String server;
+    private String api_key;
+
+    // volley
+    RequestQueue queue;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_playlist_list);
+        setContentView(R.layout.activity_playlists);
+
+
+        Intent intent = getIntent();
+
+        // Set a title for this activity
+        String title;
+        if ((title = intent.getStringExtra("title")) == null) {
+            title = "";
+        }
+        getSupportActionBar().setTitle(title);
+
+        playlist_id = intent.getStringExtra("_id");
+
+        getSettings();
+
+        // adapter for showing the list of songs
+        adapter=new SimpleAdapter(this, songs, R.layout.playlist_element,
+                new String[] {TAG_SONG_TITLE, TAG_SONG_STATUS},
+                new int[] {R.id.name, R.id.status});
+
+        // setup song view list
+        songsListView = (ListView) findViewById(R.id.playlistsList);
+        songsListView.setAdapter(adapter);
+        songsListView.setClickable(true);
+        songsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // When clicked, show a toast with the TextView text or do whatever you need.
+                HashMap<String, String> song = songs.get(position);
+                Log.d(TAG, song.get(TAG_SONG_TITLE));
+            }
+        });
+
+        swipeContainer = (SwipeRefreshLayout) findViewById(R.id.swipeContainer);
+
+        // On swipedown redownload the song list
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                Log.d(TAG, "Refresh");
+                getSongs();
+            }
+        });
+
+        queue = Volley.newRequestQueue(this);
+
+        getSongs();
+    }
+
+    private void getSongs() {
+        showSpinner();
+        JsonArrayRequest playlistsReq = new JsonArrayRequest(
+                Request.Method.GET, server + "playlists/"+playlist_id+"/songs",
+                null, // params to send to the server
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray resp) {
+                        //Log.d(TAG, resp.toString());
+                        for(int i = 0; i < resp.length(); i++) {
+                            try {
+                                JSONObject song = resp.getJSONObject(i);
+                                HashMap<String, String> plitem = new HashMap<String, String>();
+                                //
+                                plitem.put(TAG_SONG_TITLE, song.getString("title"));
+                                if (song.has(TAG_SONG_STATUS)) {
+                                    plitem.put(TAG_SONG_STATUS, song.getString("status"));
+                                } else {
+                                    plitem.put(TAG_SONG_STATUS, "Not synced");
+                                }
+                                plitem.put(TAG_SONG_ID, song.getString("_id"));
+                                songs.add(plitem);
+                                adapter.notifyDataSetChanged();
+                                swipeContainer.setRefreshing(false);
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                                swipeContainer.setRefreshing(false);
+                            }
+
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d(TAG, "Error: " + error.getMessage());
+                        swipeContainer.setRefreshing(false);
+                    }
+                });
+        queue.add(playlistsReq);
+    }
+
+    private void showSpinner() {
+        swipeContainer.post(new Runnable() {
+            @Override
+            public void run() {
+                swipeContainer.setRefreshing(true);
+            }
+        });
+    }
+
+    private void getSettings() {
+        // Loads defaults from the settings ui file
+        PreferenceManager.setDefaultValues(this, R.xml.settings, false);
+        // get the settings
+        SP = PreferenceManager.getDefaultSharedPreferences(this);
+        // load settings into local variables with defaults
+        server = SP.getString("server", getResources().getString(R.string.default_server));
+        api_key = SP.getString("api_key", "");
     }
 }
--- app/src/main/java/ee/arti/musicsync/PlaylistsActivity.java
+++ app/src/main/java/ee/arti/musicsync/PlaylistsActivity.java
@@ -3,20 +3,32 @@ package ee.arti.musicsync;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.preference.PreferenceManager;
+import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 import android.widget.SimpleAdapter;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import com.android.volley.Request;
 import com.android.volley.RequestQueue;
 import com.android.volley.Response;
 import com.android.volley.VolleyError;
+import com.android.volley.VolleyLog;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.JsonObjectRequest;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -24,9 +36,9 @@ import java.util.HashMap;
 public class PlaylistsActivity extends AppCompatActivity {
 
     private static final String TAG = "PlaylistsActivity";
-    private static final String TAG_PLAYLIST_NAME = "name";
+    private static final String TAG_PLAYLIST_TITLE = "title";
     private static final String TAG_PLAYLIST_STATUS = "status";
-    private static final String TAG_PLAYLIST_DEBUG = "debug_data";
+    private static final String TAG_PLAYLIST_ID = "_id";
 
     // contains all the playlists
     ArrayList<HashMap<String, String>> playlists = new ArrayList<HashMap<String, String>>();;
@@ -34,6 +46,8 @@ public class PlaylistsActivity extends AppCompatActivity {
     SimpleAdapter adapter;
     // playlists list view
     private ListView playlistsListView;
+
+    private SwipeRefreshLayout swipeContainer;
 
     // settings object
     private SharedPreferences SP;
@@ -51,54 +65,44 @@ public class PlaylistsActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_playlists);
 
-        // Loads defaults from the settings ui file
-        PreferenceManager.setDefaultValues(this, R.xml.settings, false);
-        // get the settings
-        SP = PreferenceManager.getDefaultSharedPreferences(this);
-        // load settings into local variables with defaults
-        server = SP.getString("server", getResources().getString(R.string.default_server));
-        api_key = SP.getString("api_key", "");
+        // get app settings
+        getSettings();
 
-        HashMap<String, String> plitem = new HashMap<String, String>();
-        // add some dummy data
-        plitem.put(TAG_PLAYLIST_NAME, "Music I like");
-        plitem.put(TAG_PLAYLIST_DEBUG, "youtube:PLB5VrND_o3PgZNzNdohFDWE5BTFIPDImQ");
-        plitem.put(TAG_PLAYLIST_STATUS, "Syncing 0/136");
-        playlists.add(plitem);
-
-        // Create the adapter that will show our playlists in a scrollable list view
+         // Create the adapter that will show our playlists in a scrollable list view
         adapter=new SimpleAdapter(this, playlists, R.layout.playlist_element,
-                new String[] {TAG_PLAYLIST_NAME, TAG_PLAYLIST_DEBUG, TAG_PLAYLIST_STATUS},
-                new int[] {R.id.name, R.id.debug_data, R.id.status});
+                new String[] {TAG_PLAYLIST_TITLE, TAG_PLAYLIST_STATUS},
+                new int[] {R.id.name, R.id.status});
 
+        // Setup playlist viewer list
         playlistsListView = (ListView) findViewById(R.id.playlistsList);
         playlistsListView.setAdapter(adapter);
+        playlistsListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                // When clicked, show a toast with the TextView text or do whatever you need.
+                HashMap<String, String> playlist = playlists.get(position);
+                Log.d(TAG, "Showing songs in " + playlist.get(TAG_PLAYLIST_TITLE));
+                Intent showPlaylist = new Intent(getApplicationContext(), PlaylistListActivity.class);
+                showPlaylist.putExtra(TAG_PLAYLIST_TITLE, playlist.get(TAG_PLAYLIST_TITLE));
+                showPlaylist.putExtra(TAG_PLAYLIST_ID, playlist.get(TAG_PLAYLIST_ID));
+                startActivity(showPlaylist);
+            }
+        });
+
+        swipeContainer = (SwipeRefreshLayout) findViewById(R.id.swipeContainer);
+
+        // On swipedown redownload playlists
+        swipeContainer.setOnRefreshListener(new SwipeRefreshLayout.OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                Log.d(TAG, "Refresh");
+                getPLaylists();
+            }
+        });
 
 
         queue = Volley.newRequestQueue(this);
-        // Request a string response from the provided URL.
-        StringRequest stringRequest = new StringRequest(Request.Method.GET, server+"playlist",
-                new Response.Listener<String>() {
-                    @Override
-                    public void onResponse(String response) {
-                        // Display the first 500 characters of the response string.
-                        Log.d(TAG+"Volley", response);
-                        HashMap<String, String> plitem = new HashMap<String, String>();
-                        // add some dummy data
-                        plitem.put(TAG_PLAYLIST_NAME, "HTTP GET "+server+"playlist");
-                        plitem.put(TAG_PLAYLIST_DEBUG, response);
-                        plitem.put(TAG_PLAYLIST_STATUS, "OK");
-                        playlists.add(plitem);
-                        adapter.notifyDataSetChanged();
-                    }
-                }, new Response.ErrorListener() {
-            @Override
-            public void onErrorResponse(VolleyError error) {
-                Log.d(TAG+"Volley","Volley didn't work");
-            }
-        });
-        // Add the request to the RequestQueue.
-        queue.add(stringRequest);
+
+        getPLaylists();
 
     }
 
@@ -106,6 +110,7 @@ public class PlaylistsActivity extends AppCompatActivity {
     public void onStart() {
         super.onStart();
         Log.d(TAG, "onStart");
+        getSettings(); // settings may have changed meantime
     }
 
     @Override
@@ -126,5 +131,58 @@ public class PlaylistsActivity extends AppCompatActivity {
                 return super.onOptionsItemSelected(item);
 
         }
+    }
+
+    private void getPLaylists() {
+        showSpinner();
+        JsonArrayRequest playlistsReq = new JsonArrayRequest(Request.Method.GET, server + "playlists", null,
+                new Response.Listener<JSONArray>() {
+                    @Override
+                    public void onResponse(JSONArray resp) {
+                        Log.d(TAG, "Got playlists");
+                        playlists.clear();
+                        for(int i = 0; i < resp.length(); i++) {
+                            try {
+                                JSONObject playlist = resp.getJSONObject(i);
+                                HashMap<String, String> plitem = new HashMap<String, String>();
+                                // add some dummy data
+                                plitem.put(TAG_PLAYLIST_TITLE, playlist.getString(TAG_PLAYLIST_TITLE));
+                                plitem.put(TAG_PLAYLIST_ID, playlist.getString(TAG_PLAYLIST_ID));
+                                plitem.put(TAG_PLAYLIST_STATUS, "OK");
+                                playlists.add(plitem);
+                                adapter.notifyDataSetChanged();
+                            } catch (JSONException e) {
+                                e.printStackTrace();
+                            }
+                        }
+                        swipeContainer.setRefreshing(false);
+                    }
+                }, new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d(TAG, "Error: " + error.getMessage());
+                        swipeContainer.setRefreshing(false);
+                    }
+                });
+        queue.add(playlistsReq);
+    }
+
+    private void showSpinner() {
+        swipeContainer.post(new Runnable() {
+            @Override
+            public void run() {
+                swipeContainer.setRefreshing(true);
+            }
+        });
+    }
+
+    private void getSettings() {
+        // Loads defaults from the settings ui file
+        PreferenceManager.setDefaultValues(this, R.xml.settings, false);
+        // get the settings
+        SP = PreferenceManager.getDefaultSharedPreferences(this);
+        // load settings into local variables with defaults
+        server = SP.getString("server", getResources().getString(R.string.default_server));
+        api_key = SP.getString("api_key", "");
     }
 }
